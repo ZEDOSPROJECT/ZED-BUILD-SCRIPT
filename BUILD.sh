b5cb30913d4ae9b6d17eb124bcac0c913efb4db3
alexandrenunes1331@gmail.com <alexandrenunes1331@gmail.com>
2018-02-22 23:16:31 +0000

    Refix

--- BUILD.sh
+++ BUILD.sh
@@ -6,25 +6,21 @@ if [[ $EUID -ne 0 ]]; then
 else
 	cd FILES
 	echo "Installing necessary packages"
-    sudo add-apt-repository ppa:nathan-renniewaldock/flux
-    sudo add-apt-repository ppa:bhdouglass/indicator-remindor
 	sudo dpkg -i pinguybuilder.deb &> /dev/null
-	sudo apt install -f -y &> /dev/null
+	sudo apt-get install -f -y &> /dev/null
 	sudo dpkg -i masalla_0.9_all.deb &> /dev/null
-	sudo apt install -f -y &> /dev/null
-    sudo dpkg -i indicator-keylock_3.1.0_amd64.deb &> /dev/null
-    sudo apt install -f -y &> /dev/null
-    sudo apt update
-	sudo apt install xclip xfwm4 xscreensaver xscreensaver-data indicator-remindor python-pyaudio shutter gambas3 mplayer fluxgui ubuntu-mate-core ubuntu-mate-desktop lightdm lightdm-gtk-greeter ubuntu-artwork  -y &> /dev/null
+	sudo apt-get install -f -y &> /dev/null
+    	sudo dpkg -i indicator-keylock_3.1.0_amd64.deb &> /dev/null
+    	sudo apt-get install -f -y &> /dev/null
+	sudo apt install xclip xfwm4 xscreensaver xscreensaver-data python-pyaudio shutter gambas3 mplayer ubuntu-mate-core ubuntu-mate-desktop lightdm lightdm-gtk-greeter ubuntu-artwork  -y &> /dev/null
 	rm -rf /usr/share/backgraunds
 	clear
 	echo "Downloading ZED OS Files"
 	wget https://zedfiles.blob.core.windows.net/updates/update.tar.gz
 	echo "Installing ZED OS Files"
 	tar -zxvf update.tar.gz -C / &> /dev/null
-	rm update.tar.gz
-    chmod -R +x /sbin
-    sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/zed/zed.plymouth 100
+	rm -zxvf update.tar.gz
+    	sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/zed/zed.plymouth 100
 	echo "Select ZED OS Plymouth afther press ENTER"
 	read
 	sudo update-alternatives --config default.plymouth
@@ -35,4 +31,3 @@ else
 	echo "ZED is prepared to build"
 	echo "To Build the ISO use Pinguy Builder"
 fi
-
